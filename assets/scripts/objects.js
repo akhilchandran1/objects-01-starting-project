d7de1731dd3f8c911a23be82e39ff88e47fa47f9
Akhil Chandran <akhil9216@gmail.com>
2022-02-03 21:33:01 +0000

    property access demo

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -9,3 +9,18 @@ const person = {
 }
 
 
+// print object property name
+console.log(person.name)
+
+// adding new property to person object
+person.gender = "male";
+console.log(person);
+
+// deleting greeting property from person object
+delete person.greeting;
+// NOTE:( person.greeting = undefined ) will delete the property just like above line of code
+console.log(person);
+
+// addind new STRING property to object
+person["monthely salary"] = 3500;
+console.log(person);
