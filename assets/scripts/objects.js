49f0e41ba21d63c50ee31907eae1ab97ddeaac42
Akhil Chandran <akhil9216@gmail.com>
2022-02-03 20:56:33 +0000

    created an object

---
+++ assets/scripts/objects.js
@@ -0,0 +1,11 @@
+const person = {
+    name: "Akhil Chandran",
+    age: 32,
+    hobbies: ['swimming', 'dansing', 'pool', 'chess'],
+    greeting: function (){
+        alert("you are years old, so you can have these hobbies");
+        
+    }
+}
+
+
