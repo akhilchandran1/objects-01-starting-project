2e4e77e13a89d10c4abf8552f1c83a4ae2c2f9a1
Akhil Chandran <akhil9216@gmail.com>
2022-02-07 13:11:21 +0000

    filter functionality added

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,6 +3,34 @@ const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
+const renderMovies = (filter = '') => {
+    const movieList = document.getElementById("movie-list");
+    
+    if (movies.length === 0){
+        movieList.classList.remove('visible');
+        return;
+    } else {
+        movieList.classList.add("visible");
+    }
+
+    movieList.innerHTML = "";
+
+    const filteredMovies = !filter? movies:movies.filter(movie => movie.info.title.includes(filter));
+
+    filteredMovies.forEach ((movie) => {
+        const movieEl = document.createElement("li");
+        movieEl.textContent = movie.info.title;
+        let text = movie.info.title + '-';
+        for (const key in movie.info) {
+            if (key !== 'title') {
+                text = text+ `${key} : ${movie.info[key]}`
+            }
+        }
+        movieEl.textContent = text;
+        movieList.append(movieEl);
+    });
+}
+
 const addMovieHandler = () => {
     const title = document.getElementById("title").value.trim();
     const extraName = document.getElementById("extra-name").value.trim();
@@ -20,8 +48,16 @@ const addMovieHandler = () => {
             id: Math.random()
         };
         movies.push(newMovie);
+        renderMovies();
         console.log(movies);
     }
 };
 
+const searchMovieHandler = () => {
+    const filterTerm = document.getElementById('filter-title').value;
+    renderMovies(filterTerm);
+
+}
+
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
